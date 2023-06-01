60ae27436cf77da313c58918d49aad177dea9ae9
Michael Gwilt <mgwilt@users.noreply.github.com>
2023-06-01 15:44:09 -0700

    first test

---
+++ Program.cs
@@ -0,0 +1,121 @@
+﻿using YamlDotNet.Serialization;
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Presentation;
+using DocumentFormat.OpenXml.Packaging;
+
+var yamlConfigPath = "./test.yaml";
+var pptxOutputPath = "./output/todo-makethisavariable.pptx";
+
+
+var yamlConfig = await File.ReadAllTextAsync(yamlConfigPath);
+var config = new Deserializer().Deserialize<PowerPointConfig>(yamlConfig);
+
+PowerPointGenerator.Generate(config, pptxOutputPath);
+
+public static class PowerPointGenerator
+{
+    public static void Generate(PowerPointConfig config, string outputPath)
+    {
+        using var presentation = PresentationDocument.Create(outputPath, PresentationDocumentType.Presentation);
+        var presentationPart = presentation.AddPresentationPart();
+        presentationPart.Presentation = new Presentation();
+        var slideIdList = new SlideIdList();
+
+        foreach (var slideConfig in config.Slides)
+        {
+            var slidePart = AddSlide(presentationPart, slideConfig);
+            var slideId = new SlideId { Id = (UInt32Value) (101U + slideIdList.ChildElements.Count), RelationshipId = presentationPart.GetIdOfPart(slidePart) };
+            slideIdList.Append(slideId);
+        }
+
+        presentationPart.Presentation.Append(slideIdList);
+        presentationPart.Presentation.Save();
+    }
+
+    private static SlidePart AddSlide(PresentationPart presentationPart, PowerPointConfig.SlideConfig slideConfig)
+    {
+        var slidePart = presentationPart.AddNewPart<SlidePart>();
+        var slide = new Slide(new CommonSlideData(new ShapeTree()));
+        var titleShape = CreateTitleShape(slideConfig.Title);
+        var contentShape = CreateContentShape(slideConfig.Content);
+        
+        if (!string.IsNullOrEmpty(slideConfig.ImagePath))
+        {
+            var imagePart = slidePart.AddImagePart(ImagePartType.Png);
+            using var stream = new FileStream(slideConfig.ImagePath, FileMode.Open);
+            imagePart.FeedData(stream);
+            
+            var imageShape = CreateImageShape(slidePart, imagePart);
+            slide.CommonSlideData.ShapeTree.AppendChild(imageShape);
+        }
+
+        slide.CommonSlideData.ShapeTree.AppendChild(titleShape);
+        slide.CommonSlideData.ShapeTree.AppendChild(contentShape);
+        slidePart.Slide = slide;
+        return slidePart;
+    }
+
+
+    private static Shape CreateTitleShape(string title)
+    {
+        var shape = new Shape();
+        var textBody = new TextBody();
+        var paragraph = new DocumentFormat.OpenXml.Drawing.Paragraph();
+        var run = new DocumentFormat.OpenXml.Drawing.Run();
+        var text = new Text(title);
+        
+        run.Append(text);
+        paragraph.Append(run);
+        textBody.Append(paragraph);
+        shape.Append(textBody);
+        
+        return shape;
+    }
+
+    private static Shape CreateContentShape(string content)
+    {
+        var shape = new Shape();
+        var textBody = new TextBody();
+        var paragraph = new DocumentFormat.OpenXml.Drawing.Paragraph();
+        var run = new DocumentFormat.OpenXml.Drawing.Run();
+        var text = new Text(content);
+        
+        run.Append(text);
+        paragraph.Append(run);
+        textBody.Append(paragraph);
+        shape.Append(textBody);
+        
+        return shape;
+    }
+
+    private static Picture CreateImageShape(SlidePart slidePart, ImagePart imagePart)
+    {
+        var picture = new Picture();
+        var nonVisualPictureProperties = new NonVisualPictureProperties();
+        var blipFill = new BlipFill();
+        var blip = new DocumentFormat.OpenXml.Drawing.Blip {Embed = slidePart.GetIdOfPart(imagePart)};
+        var stretch = new DocumentFormat.OpenXml.Drawing.Stretch();
+        var fillRectangle = new DocumentFormat.OpenXml.Drawing.FillRectangle();
+
+        stretch.Append(fillRectangle);
+        blipFill.Append(blip);
+        blipFill.Append(stretch);
+        picture.Append(nonVisualPictureProperties);
+        picture.Append(blipFill);
+
+        return picture;
+    }
+}
+
+
+public class PowerPointConfig
+{
+    public List<SlideConfig> Slides { get; set; }
+
+    public class SlideConfig
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# power-point-generator
+# PowerPoint Generator
+
+This is a simple tool for generating PowerPoint presentations from a YAML configuration.
+
+## How to Run
+
+1. Build the application.
+
+2. Create a YAML configuration file following the example provided in `sample_config.yaml`.
+
+3. Run the application with the following command: `dotnet run -- path/to/your_config.yaml output.pptx`
+
+The PowerPoint presentation will be generated at the specified output path.
